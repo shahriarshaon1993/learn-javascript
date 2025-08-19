87e657505daea65a40896762b1d27a779740b191
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-08-19 21:11:42 +0600

    feat(Implicit Binding): Example Three

--- this.js
+++ this.js
@@ -1,21 +1,12 @@
-let printPlayerNameFunction = function (obj) {
-    obj.printPlayerName = function () {
-        console.log(this.name);
+const Person = function (name, age) {
+    return {
+        name: name,
+        age: age,
+        printName: function () {
+            console.log(this.name);
+        },
     };
 };
 
-const sakib = {
-    name: "Sakib",
-    age: 35,
-};
-
-const tamim = {
-    name: "Tamim",
-    age: 40,
-};
-
-printPlayerNameFunction(sakib);
-printPlayerNameFunction(tamim);
-
-sakib.printPlayerName();
-tamim.printPlayerName();
+const sakib = Person("Sakib", 35);
+sakib.printName();
