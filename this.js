b2c10231dbe00e32db77e393e28a592deb0e831a
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-08-19 21:06:00 +0600

    feat(Implicit Binding): Example Two

--- this.js
+++ this.js
@@ -1,9 +1,21 @@
+let printPlayerNameFunction = function (obj) {
+    obj.printPlayerName = function () {
+        console.log(this.name);
+    };
+};
+
 const sakib = {
     name: "Sakib",
     age: 35,
-    printPlayerName: function () {
-        console.log(this.name);
-    },
 };
 
+const tamim = {
+    name: "Tamim",
+    age: 40,
+};
+
+printPlayerNameFunction(sakib);
+printPlayerNameFunction(tamim);
+
 sakib.printPlayerName();
+tamim.printPlayerName();
