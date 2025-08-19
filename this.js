8304552ff850baaf908326ce40b2801082c07fc7
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-08-19 21:15:46 +0600

    feat(Implicit Binding): Example Four

--- this.js
+++ this.js
@@ -5,8 +5,15 @@ const Person = function (name, age) {
         printName: function () {
             console.log(this.name);
         },
+        father: {
+            name: "Mr. John Deo",
+            printName: function () {
+                console.log(this.name);
+            },
+        },
     };
 };
 
 const sakib = Person("Sakib", 35);
 sakib.printName();
+sakib.father.printName(); // Mr. John Deo
